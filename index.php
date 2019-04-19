d807648a7fe99c04533aa8f85f973efe7a02f60f
tlm-2501 <13thSlayer@gmail.com>
2019-04-19 18:36:16 +0300

    Add styling

--- index.php
+++ index.php
@@ -1,16 +1,31 @@
+<style>
+body {
+  color: gray;
+  background-color: #3d1600;
+  max-width: 640px;
+  margin-left: auto;
+  margin-right: auto;
+}
+input[type=text] {
+  width: 640px;
+  display: block;
+}
+input[type=submit] {
+  margin-left: 250px;
+}
+</style>
 <form action='/test.php' method='post'>
-<input type='text' name='findstr'>
-<input type='submit'>
-</form>
-Type some text from Umineko no Naku Koro ni to figure out the song that plays at this moment. Tips for usage:
-<ul>
-  <li>This works only with the official translation that is published by MangaGamer, and with 07th Mod.
-    This won't work if you're reading the old Witch Hunt translation, UmiTweak, or Umineko Project.</li>
-  <li>This works with both Question Arcs and Answer Arcs</li>
-  <li>Pieces of text that are too long may not work (due to formatting commands appearing in the middle).</li>
-  <li>Pieces of text that are too short may end up with the wrong song result because they are not unique.</li>
-  <li>
-    Generally, a piece of text corresponding to a single voice clip should work, but it doesn't have to even be a complete sentence.
-    Sequences of about 5 words tend to work correctly. If the song that is found doesn't seem correct, try searching for a longer piece of text or just scroll forward/backward a bit and try a different one.
-  </li>
+    <input type='text' name='findstr'>
+    <input type='submit'>
+    </form>
+    Type some text from Umineko no Naku Koro ni to figure out the song that plays at this moment. Notes:
+    <ul>
+      <li>This works only with the official translation that is released on Steam.
+        This won't work if you're reading the old Witch Hunt translation, UmiTweak, or Umineko Project.</li>
+      <li>Pieces of text that are too long may not work (due to formatting commands appearing in the middle).
+        Generally, a piece of text corresponding to a single voice clip should work, but it doesn't have to even be a complete sentence.
+        Sequences of about 5 words tend to work correctly.</li>
+      <li>Pieces of text that are too short may end up with the wrong song result because they are not unique.
+        If the song that is found doesn't seem correct, try searching for a longer piece of text or just scroll forward/backward a bit and try a different one.
+      </li>
 </ul>
