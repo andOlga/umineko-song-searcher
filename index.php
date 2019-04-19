0257a49b908efb9188740b74b9747c6a05b7840d
tlm-2501 <13thSlayer@gmail.com>
2019-04-19 22:35:10 +0300

    Rework user experience to use fetch instead of forms

--- index.php
+++ index.php
@@ -1,35 +1,61 @@
 <style>
-body {
-  color: gray;
-  background-color: #3d1600;
-  max-width: 640px;
-  margin-left: auto;
-  margin-right: auto;
-}
-input[type=text] {
-  width: 100%;
-  display: block;
-}
-input[type=submit] {
-  margin-left: 40%;
-}
+  body {
+    color: gray;
+    background-color: #3d1600;
+    max-width: 640px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  #findstr {
+    width: 100%;
+    display: block;
+  }
+
+  #submit {
+    margin-left: 43.5%;
+  }
+
+  #result {
+    color: red;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
 </style>
-<form action='/test.php' method='post'>
-    <input type='text' name='findstr'>
-    <input type='submit'>
-    </form>
-    Type some text from Umineko no Naku Koro ni to figure out the song that plays at this moment. Notes:
-    <ul>
-      <li>This works only with the official translation that is published by Mangagamer, with or without the 07th Mod patch.
-        This won't work if you're reading the old Witch Hunt translation patch, UmiTweak, or Umineko Project
-        due to translation differences.</li>
-      <li>Pieces of text that are too long may not work due to formatting commands appearing in the middle.</li>
-      <li>Pieces of text that are too short may end up with the wrong song result because they are not unique.</li>
-      <li>If the song that is found doesn't seem correct, try searching for a longer piece of text or just scroll forward/backward a bit in the game and try a different piece.</li>
-      <li>
-          Generally, a piece of text corresponding to a single voice clip in 07th Mod should work. It may or may not be a complete sentence.
-      </li>
-      <li>
-          Sequences of about 5 words, give or take, tend to work correctly.
-      </li>
-</ul>
+<script>
+  document.addEventListener('DOMContentLoaded', event => {
+    document.getElementById('submit').addEventListener('click', event => {
+      let fd = new FormData()
+      fd.append('findstr', document.getElementById('findstr').value)
+      fetch('/search.php', {
+        method: 'POST',
+        body: fd
+      }).then(response => response.text().then(t => {
+        let result = document.getElementById('result')
+        result.innerText = `Search result: ${t}`
+      }))
+    })
+  })
+</script>
+<div>
+  <input type='text' id='findstr'>
+  <button id='submit' type='button'>Search</button>
+</div>
+<div id='result'></div>
+<div>
+  Type some text from Umineko no Naku Koro ni to figure out the song that plays at this moment. Notes:
+  <ul>
+    <li>This works only with the official translation that is published by Mangagamer, with or without the 07th Mod patch.
+      This won't work if you're reading the old Witch Hunt translation patch, UmiTweak, or Umineko Project
+      due to translation differences.</li>
+    <li>Pieces of text that are too long may not work due to formatting commands appearing in the middle.</li>
+    <li>Pieces of text that are too short may end up with the wrong song result because they are not unique.</li>
+    <li>If the song that is found doesn't seem correct, try searching for a longer piece of text or just scroll forward/backward a bit in the game and try a different piece.</li>
+    <li>
+      Generally, a piece of text corresponding to a single voice clip in 07th Mod should work. It may or may not be a complete sentence.
+    </li>
+    <li>
+      Sequences of about 5 words, give or take, tend to work correctly.
+    </li>
+  </ul>
+</div>
