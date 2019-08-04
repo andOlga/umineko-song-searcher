362c018d7439bb0ae3c4adc62bf379e61d41dd62
tlm-2501 <13thSlayer@gmail.com>
2019-08-04 14:36:20 +0300

    Cache files locally for faster search

--- search.php
+++ search.php
@@ -1,11 +1,20 @@
 <?php
-$questionContents = file_get_contents(
-    'http://07th-mod.com/download.php?repository=umineko-question&file=master/InDevelopment/ManualUpdates/0.utf'
-);
-$answerContents = file_get_contents(
-    'http://07th-mod.com/download.php?repository=umineko-answer&file=adv_mode/0.utf'
-);
-$contents =  $questionContents . $answerContents;
+$fileMap = [
+    '0question.u' => 'http://07th-mod.com/download.php?repository=umineko-question&file=master/InDevelopment/ManualUpdates/0.utf',
+    '0answer.u' => 'http://07th-mod.com/download.php?repository=umineko-answer&file=adv_mode/0.utf'
+];
+$contents = '';
+
+foreach ($fileMap as $file => $url) {
+    if (file_exists($file)) {
+        $contents .= file_get_contents($file);
+    } else {
+        $newContents = file_get_contents($url);
+        $contents .= $newContents;
+        file_put_contents($file, $newContents);
+    }
+}
+
 $matches = preg_split('/bgm(1v?|play2?)/', $contents);
 foreach ($matches as $match) {
     if (stripos($match, $_POST['findstr']) !== false) {
