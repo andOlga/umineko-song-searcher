da1379a4900255115423883db62efb142d3d0a90
tlm-2501 <13thSlayer@gmail.com>
2019-08-04 14:27:48 +0300

    Fetch script from 07th-mod.com

--- search.php
+++ search.php
@@ -1,5 +1,11 @@
 <?php
-$contents = file_get_contents('0.u') . file_get_contents('0answer.u');
+$questionContents = file_get_contents(
+    'http://07th-mod.com/download.php?repository=umineko-question&file=master/InDevelopment/ManualUpdates/0.utf'
+);
+$answerContents = file_get_contents(
+    'http://07th-mod.com/download.php?repository=umineko-answer&file=adv_mode/0.utf'
+);
+$contents =  $questionContents . $answerContents;
 $matches = preg_split('/bgm(1v?|play2?)/', $contents);
 foreach ($matches as $match) {
     if (stripos($match, $_POST['findstr']) !== false) {
