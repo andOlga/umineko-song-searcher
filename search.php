1ed6025f4ac1012ebdb700575a58a7f727e816b0
ooa113y <13thSlayer@gmail.com>
2020-10-06 03:02:04 +0300

    Add full song title display (thanks to Umineko Project)

--- search.php
+++ search.php
@@ -1,7 +1,8 @@
 <?php
 $fileMap = [
     '0question.u' => 'http://07th-mod.com/download.php?repository=umineko-question&file=master/InDevelopment/ManualUpdates/0.utf',
-    '0answer.u' => 'http://07th-mod.com/download.php?repository=umineko-answer&file=adv_mode/0.utf'
+    '0answer.u' => 'http://07th-mod.com/download.php?repository=umineko-answer&file=adv_mode/0.utf',
+    'bgmtitles' => 'https://raw.githubusercontent.com/ooa113y/umiproj-wh-tl/master/script/wh/code.txt'
 ];
 $contents = '';
 
@@ -19,9 +20,11 @@ $matches = preg_split('/bgm(1v?|play2?)/', $contents);
 foreach ($matches as $match) {
     if (stripos($match, $_POST['findstr']) !== false) {
         $subMatches = [];
+        $titles = [];
         $thing = trim(preg_split('/\n|\,/', $match)[0]);
         preg_match("/BGM_s_Ch = $thing\s+mov.+\"(.+)\"/", $contents, $subMatches);
-        echo $subMatches[1];
+        preg_match("/if %BGM_Id == $thing\s+mov.+\"(.+)\"/", $contents, $titles);
+        echo $subMatches[1] . " (" . $titles[1] . ")"
         break;
     }
 }
