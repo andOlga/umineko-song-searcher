f970bc51f854187bca42b92f0e339df5360f804b
&Olga <462484+andOlga@users.noreply.github.com>
2022-11-04 11:47:03 +0400

    Specify Python version

--- serve.bat
+++ serve.bat
@@ -1,12 +1,12 @@
 echo off
 cls
-python -V 2> nul
+python3 -V 2> nul
 if errorlevel 1 goto setup
-start /min "" python -m http.server
+start /min "" python3 -m http.server
 start http://localhost:8000
 exit
 :setup
 echo We need to run some initial setup. Please press any key, click the "Install" button in the window that shows,
 echo and then run this script again after it's done.
 pause > nul
-python
+python3
--- serve.sh
+++ serve.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-python -m http.server
+python3 -m http.server
 xdg-open http://localhost:8000
